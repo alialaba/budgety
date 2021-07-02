0a69495c56dfee36cb3a04965e75f2042f2b2f32
alialaba <alialaba079@gmail.com>
2021-07-02 11:37:00 -0700

    refactored the Dom for addbtn

--- js/index.js
+++ js/index.js
@@ -9,27 +9,34 @@ let UIController = (function () {
     let DOMStrings = {
         inputType: ".add__type",
         inputDecription: ".add__description",
-        inputValue: ".add__value"
+        inputValue: ".add__value",
+        addBtn: ".add__btn"
     }
 
 
     //getting the inputs value
     return {
         getInputs: function () {
-            //returning object of  the  inputs value
+            //method for returning object of  the  inputs value
             return {
                 type: document.querySelector(DOMStrings.inputType).value,
                 description: document.querySelector(DOMStrings.inputDecription).value,
                 value: document.querySelector(DOMStrings.inputValue).value,
             }
+        },
+        //method for getting access to DOMstring
+        getDOMStings: function () {
+            return DOMStrings;
         }
-
     }
 
 })()
 
 //GLOBAL APP CONTROLLER
 let controller = (function (budgetCtrl, UICtrl) {
+
+    //getting acces to the DOMstring in UICtrl
+    let DOM = UICtrl.getDOMStings();
 
     //function that add items
     const addItemCtrl = () => {
@@ -45,7 +52,7 @@ let controller = (function (budgetCtrl, UICtrl) {
         //display the budget  on the UI
     }
     //button variable
-    let addBtn = document.querySelector(".add__btn");
+    let addBtn = document.querySelector(DOM.addBtn);
     //the button click event 
     addBtn.addEventListener("click", addItemCtrl);
 
