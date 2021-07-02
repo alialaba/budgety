2b0ef1301893c174697f526781a106b1e2bb7778
alialaba <alialaba079@gmail.com>
2021-07-02 10:47:29 -0700

    added module patterns

---
+++ js/index.js
@@ -0,0 +1,16 @@
+//BUDGET CONTROLLER
+let budgetCntroller = (function () {
+
+})();
+
+//UI CONTROLLER
+let UIController = (function () {
+
+})()
+
+//GLOBAL APP CONTROLLER
+let controller = (function () {
+
+})();
+
+
